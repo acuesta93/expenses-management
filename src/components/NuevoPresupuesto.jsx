38ec0aac59e70951e023de8a7bab53e8a36033c4
Ángel <angelcuestamargallo@gmail.com>
2022-12-14 18:31:36 +0100

    Added components

---
+++ src/components/NuevoPresupuesto.jsx
@@ -0,0 +1,44 @@
+import {useState} from 'react'
+import Mensaje from './Mensaje'
+
+const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
+
+    const[mensaje, setMensaje] = useState('')
+    
+    const handlePresupuesto = (e) => {
+        e.preventDefault();
+        if(!Number(presupuesto) || Number(presupuesto) < 0){
+            setMensaje('No es un presupuesto válido')
+        } else {
+            console.log('Es un presupuesto válido')
+        }
+        
+    }
+    
+    return (
+        <div className='contenedor-presupuesto contenedor sombra'>
+            <form onSubmit={handlePresupuesto} className='formulario'>
+                <div className='campo'>
+                    <label>Definir Presupuesto</label>
+
+                    <input
+                        className='nuevo-presupuesto'
+                        type="text"
+                        placeholder='Añade un Presupuesto' 
+                        value={presupuesto}
+                        onChange ={ (e) => setPresupuesto(e.target.value)}
+                    />
+                </div>
+
+                <input
+                    type="submit"
+                    value="Añadir"
+                />
+
+                {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
+            </form>
+        </div>
+    )
+}
+
+export default NuevoPresupuesto

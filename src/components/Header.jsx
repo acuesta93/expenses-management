a8db67bf97f57153ca30f3fd1e3edfa4a7ecf4d1
Ángel <angelcuestamargallo@gmail.com>
2023-03-28 21:14:10 +0200

    Added card with expenses

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import NuevoPresupuesto from './NuevoPresupuesto'
 import ControlPresupuesto from './ControlPresupuesto'
 
-const Header = ({presupuesto, setPresupuesto, isValidPresupuesto, setIsValidPresupuesto}) => {
+const Header = ({gastos, presupuesto, setPresupuesto, isValidPresupuesto, setIsValidPresupuesto}) => {
   return (
     <header>
         <h1>
@@ -10,6 +10,7 @@ const Header = ({presupuesto, setPresupuesto, isValidPresupuesto, setIsValidPres
         </h1>
         {isValidPresupuesto ? (
           <ControlPresupuesto 
+            gastos = {gastos}
             presupuesto = {presupuesto}
           />
         ) : (
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,8 +1,36 @@
 import CerrarModal from '../img/cerrar.svg'
+import { useState } from 'react'
+import Mensaje from './Mensaje'
 
-const Modal = ({setModal}) => {
+const Modal = ({setModal, animarModal, setAnimarModal, guardarGasto}) => {
+
+    const [mensaje, setMensaje] = useState('')
+
+    const [nombre, setNombre] = useState('')
+    const [cantidad, setCantidad] = useState('')
+    const [categoria, setCategoria] = useState('')
+
     const OcultarModal =() => {
-        setModal(false)
+        setAnimarModal(false)
+
+        setTimeout(() => {
+            setModal(false)
+        }, 500)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if([nombre, cantidad, categoria].includes('')){
+            setMensaje('Todos los campos son obligatorios.')
+
+            setTimeout(() => {
+                setMensaje('')
+            }, 3000);
+
+            return;
+        }
+
+        guardarGasto({nombre, cantidad, categoria})
     }
 
   return (
@@ -14,6 +42,69 @@ const Modal = ({setModal}) => {
                 onClick = {OcultarModal}
             />
         </div>
+
+        <form 
+            onSubmit={handleSubmit}
+            className={`formulario ${animarModal ? "animar" : "cerrar"}`}>
+            <legend>
+                Nuevo Gasto
+            </legend>
+            {mensaje && <Mensaje tipo ="error">{mensaje}</Mensaje>}
+
+            <div className='campo'>
+                <label htmlFor='nombre'>
+                    Nombre Gasto
+                </label>
+
+                <input 
+                    id = "nombre"
+                    type = "text"
+                    placeholder='Añade el nombre del gasto.'
+                    value = {nombre}
+                    onChange = { e => setNombre(e.target.value)}
+                />
+            </div>
+            
+            <div className='campo'>
+                <label htmlFor='cantidad'>
+                    Cantidad
+                </label>
+
+                <input 
+                    id = "cantidad"
+                    type = "number"
+                    placeholder='Añade la cantidad del gasto: ej. 300 .'
+                    value = {cantidad}
+                    onChange = { e => setCantidad(Number(e.target.value))}
+                />
+            </div>
+
+            <div className='campo'>
+                <label htmlFor='categoria'>
+                    Categoría
+                </label>
+
+                <select
+                    id='categoria'
+                    value = {categoria}
+                    onChange = { e => setCategoria(e.target.value)}
+                >
+                    <option value="">-- Seleccione --</option>
+                    <option value="ahorro">Ahorro</option>
+                    <option value="comida">Comida</option>
+                    <option value="casa">Casa</option>
+                    <option value="gastos">Gastos varios</option>
+                    <option value="ocio">Ocio</option>
+                    <option value="salud">Salud</option>
+                    <option value="suscripciones">Suscripciones</option>
+                </select>
+            </div>
+
+            <input
+                type="submit"
+                value="Añadir Gasto"
+            />
+        </form>
     </div>
   )
 }
